e789b47680978e560796ab93cb9397d1c0fd3111
Ashraf Ali Hassan <writeittoashraf@gmail.com>
2021-06-20 20:40:30 +0530

    The changed code

---
+++ main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+# dfs = pd.read_html('https://en.m.wikipedia.org/wiki/List_of_members_of_the_17th_Lok_Sabha')
+# for df in dfs:
+#     print(df)
+url = 'https://en.m.wikipedia.org/wiki/List_of_members_of_the_17th_Lok_Sabha'
+r = requests.get(url)
+if r.status_code == 200:  #checks the status code
+    soup = BeautifulSoup(r.text, 'html.parser')
+    table_ = soup.find('table', class_ = 'wikitable sortable')
+    # headers = []
+    # for i in table_.find_all('tbody'):
+    #     title = i.text.strip()
+    #     headers.append(title)
+    # print(headers)
+    body = table_.find_all('tbody') #finds the body of the table
+    for team in body:
+        for i in range(0,3):
+            rows = team.find_all('tr')
+            # head = team.find_all('th')
+            print(rows)
