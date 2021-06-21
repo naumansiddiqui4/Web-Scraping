a107d4de69b74275e9dd6d0f9332c76427731084
Ashraf Ali Hassan <writeittoashraf@gmail.com>
2021-06-21 22:14:37 +0530

    The New Commit

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import pandas as pd
+import csv
 import requests
 from bs4 import BeautifulSoup
-# dfs = pd.read_html('https://en.m.wikipedia.org/wiki/List_of_members_of_the_17th_Lok_Sabha')
-# for df in dfs:
-#     print(df)
-url = 'https://en.m.wikipedia.org/wiki/List_of_members_of_the_17th_Lok_Sabha'
-r = requests.get(url)
-if r.status_code == 200:  #checks the status code
-    soup = BeautifulSoup(r.text, 'html.parser')
-    table_ = soup.find('table', class_ = 'wikitable sortable')
-    # headers = []
-    # for i in table_.find_all('tbody'):
-    #     title = i.text.strip()
-    #     headers.append(title)
-    print()
-    body = table_.find_all('tbody') #finds the body of the table
-    for team in body:
-        for i in range(0,3):
-            rows = team.find_all('tr')
-            # head = team.find_all('th')
-            print(rows)
+def csvfunc(x):
+    c = 0
+    length = len(x)
+    with open('store.csv', 'w', newline='') as csvfile:
+        fieldnames = ['S.N','Name','Constituency','Party']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for j in range(1,length):
+            while x != []:
+                c = c+1
+                writer.writerow({'S.N':c, 'Name':x[j-1], 'Constituency':x[j], 'Party':x[j+1]})
+                x =x[3:]
+def main():
+    y = []
+    url = 'https://en.m.wikipedia.org/wiki/List_of_members_of_the_17th_Lok_Sabha'
+    r = requests.get(url)
+    if r.status_code == 200:  #checks the status code
+        soup = BeautifulSoup(r.text, 'html.parser')
+        table_ = soup.find('table', class_ = 'wikitable').next
+        row = table_.find_all('tr') #finds the body of the table
+        for team in range(0,len(row)):
+            head = row[team].find_all('td')
+            for i in head:
+                # print(i.text)
+                y.append((i.text.strip()))
+        print(y)
+        csvfunc(y)
+if __name__ == "__main__":
+    main()
