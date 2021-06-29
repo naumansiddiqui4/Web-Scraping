64855676628478a25b10537adf0275aef79abbef
Ashraf Ali Hassan <writeittoashraf@gmail.com>
2021-06-29 23:15:36 +0530

    Added the session details

--- main.py
+++ main.py
@@ -1,32 +1,55 @@
-import pandas as pd
 import csv
 import requests
+from dateutil.parser import parse
 from bs4 import BeautifulSoup
+def _date(word):
+    word = str(word)
+    date = word.split('on')[1].lstrip().split(')')[0]
+    date = str(parse(date))
+    return date
 def csvfunc(x):
+    cnst = ''
+    name = ''
+    party = ''
     c = 0
-    nm = []
-    cn = []
-    pt = []
-    for n in range(0, 1000, 3):
-        nm.append(n)
-        cn.append((n + 1))
-        pt.append((n + 2))
     length = len(x)
+    nm = [] #list for name
+    cn = [] #list for constituency
+    pt = [] #list for party
+    st = [] #list for start
+    ed = [] #list for end
+    for n in range(0, length, 5):
+        cn.append(n)
+        nm.append((n + 1))
+        pt.append((n + 2))
+        st.append((n + 3))
+        ed.append((n + 4))
+
     with open('store.csv', 'w', newline='') as csvfile:
-        fieldnames = ['S.N','Constituency','Name','Party','State','Twitter', 'Politwoops']
+        fieldnames = ['S.N','Constituency','Name','Party','State', 'Term_Start', 'Term_End','Twitter', 'Politwoops']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        for j in range(0,length):
-            if (j in nm):
-                cnst = x[j]
-            elif (j in cn):
-                name = x[j]
-            elif (j in pt):
-                party = x[j]
-                c += 1
-                writer.writerow({'S.N':c,'Constituency':cnst,'Name':name, 'Party':party})
+        for j in range(0, length):
+                if j in cn:
+                    cnst = x[j]
+                elif j in nm:
+                    name = x[j]
+                elif j in pt:
+                    party = x[j]
+                elif j in st:
+                    start = x[j]
+                else:
+                    end = x[j]
+                    c += 1
+                    writer.writerow({'S.N':c,'Constituency':cnst,'Name':name, 'Party':party, 'State':'Andhra Pradesh', 'Term_Start':start, 'Term_End':end, 'Twitter':False, 'Politwoops':False})
 def scrape():
-    y = []
+    str_lst = []
+    end_lst = []
+    for n in range(3, 1000000, 5):
+        str_lst.append(n)
+    y = []*1000000
+    k = []*1000000
+    val = ''
     url = 'https://en.m.wikipedia.org/wiki/List_of_members_of_the_17th_Lok_Sabha'
     r = requests.get(url)
     if r.status_code == 200:  #checks the status code
@@ -36,9 +59,35 @@ def scrape():
         for team in range(0,len(row)):
             head = row[team].find_all('td')
             for i in head:
-                # print(i.text)
-                y.append((i.text.strip()))
-        print(y)
-        csvfunc(y)
+                if '' != i.text.strip():
+                    y.append(i.text.strip())
+        for i in range(0,len(y)):
+            if '(Elected on' in y[i]:
+                val = y[i-3]
+                y.insert(i,val)
+                break
+        while ("" in y):
+            y.remove("")
+        y.append('')
+        y.append('')
+        k.append(y[0])
+        for i in range(1,len(y)):
+            if i%3 == 0:
+                k.append('2019-05-01')
+                k.append('Still in Parliament')
+                k.append(y[i])
+            else:
+                k.append(y[i])
+        pos = ''
+        ele = ''
+        for i in range(0,len(k)):
+            if 'died on' in k[i]:
+                k[i+3] = _date(k[i])
+                ele = _date(k[i+5])
+                pos = (i+7)
+                k.insert(pos,ele)
+                k.pop(i+8)
+        print(k)
+        csvfunc(k)
 if __name__ == "__main__":
     scrape()
